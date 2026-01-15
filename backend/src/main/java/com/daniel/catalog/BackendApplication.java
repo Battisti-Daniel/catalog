36042aa6e65cf05043c1f627b7ed762baebe0d18
Battisti-Daniel <danielbattisti@outlook.com.br>
2026-01-15 15:14:09 -0300

    Project created

--- backend/src/main/java/com/daniel/catalog/BackendApplication.java
+++ backend/src/main/java/com/daniel/catalog/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CatalogApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
